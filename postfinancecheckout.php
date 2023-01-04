15ef245b1cd9ec416fb69f188b3e88647a1f6c26
Drew Rowan <andrew.rowan@wallee.com>
2023-01-04 10:51:57 +0100

    Release 1.2.34

--- postfinancecheckout.php
+++ postfinancecheckout.php
@@ -6,7 +6,7 @@
  * This Prestashop module enables to process payments with PostFinance Checkout (https://postfinance.ch/en/business/products/e-commerce/postfinance-checkout-all-in-one.html).
  *
  * @author customweb GmbH (http://www.customweb.com/)
- * @copyright 2017 - 2022 customweb GmbH
+ * @copyright 2017 - 2023 customweb GmbH
  * @license http://www.apache.org/licenses/LICENSE-2.0 Apache Software License (ASL 2.0)
  */
 
@@ -32,7 +32,7 @@ class PostFinanceCheckout extends PaymentModule
         $this->author = 'Customweb GmbH';
         $this->bootstrap = true;
         $this->need_instance = 0;
-        $this->version = '1.2.33';
+        $this->version = '1.2.34';
         $this->displayName = 'PostFinance Checkout';
         $this->description = $this->l('This PrestaShop module enables to process payments with %s.');
         $this->description = sprintf($this->description, 'PostFinance Checkout');
--- views/templates/admin/hook/admin_order_left.tpl
+++ views/templates/admin/hook/admin_order_left.tpl
@@ -4,7 +4,7 @@
  * This Prestashop module enables to process payments with PostFinance Checkout (https://postfinance.ch/en/business/products/e-commerce/postfinance-checkout-all-in-one.html).
  *
  * @author customweb GmbH (http://www.customweb.com/)
- * @copyright 2017 - 2022 customweb GmbH
+ * @copyright 2017 - 2023 customweb GmbH
  * @license http://www.apache.org/licenses/LICENSE-2.0 Apache Software License (ASL 2.0)
 *}
 <div id="postFinanceCheckoutTransactionInfo" class="card">
@@ -34,7 +34,7 @@
 				<dd>{$failureReason|escape:'html':'UTF-8'}</dd>
 			{/if}
         		<dt>{l s='Authorization Amount' mod='postfinancecheckout'}</dt>
-				<dd>{displayPrice price=$authorizationAmount}</dd>
+				<dd>{displayPrice currency=$currency->id price=$authorizationAmount}</dd>
 				<dt>{l s='Transaction' mod='postfinancecheckout'}</dt>
 				<dd>
 					<a href="{$transactionUrl|escape:'html'}" target="_blank">
