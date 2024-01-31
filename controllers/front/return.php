9aeeda94a2fe96e3892ecab0fa264ae03c107b35
Edgaras <edgaras.tkacenka@wallee.com>
2024-01-31 11:40:40 +0200

    Release 1.2.41

--- controllers/front/return.php
+++ controllers/front/return.php
@@ -99,8 +99,11 @@ class PostFinanceCheckoutReturnModuleFrontController extends ModuleFrontControll
         }
         
         $order->setCurrentState(Configuration::get(PostFinanceCheckoutBasemodule::CK_STATUS_FAILED));
-        
-	// Set cart to cookie
+        //the new state should be saved here to override the stock issues of incrementing or decrementing items when going through the webhooks
+        //by calling the setCurrentState function more than once without saving. 
+        $order->save();
+
+        // Set cart to cookie
         $originalCartId = PostFinanceCheckoutHelper::getOrderMeta($order, 'originalCart');
         if (! empty($originalCartId)) {
             $this->context->cookie->id_cart = $originalCartId;
